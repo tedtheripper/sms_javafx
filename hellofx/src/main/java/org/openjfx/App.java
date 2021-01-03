68fe39373a01330b45dd7314cc9a262419cfa61f
tedtheripper <lemon.juice0731@gmail.com>
2021-01-03 20:35:15 +0100

    created menu bar controller

--- hellofx/src/main/java/org/openjfx/App.java
+++ hellofx/src/main/java/org/openjfx/App.java
@@ -1,6 +1,9 @@
 package org.openjfx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,17 +24,23 @@ import java.util.List;
 public class App extends Application {
 
     private static Scene scene;
+    private MenuBarController menuBarController;
+
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         //scene = new Scene(loadFXML("primary"), 640, 480);
         primaryStage.setTitle("NoteCrash");
 
+        menuBarController = new MenuBarController();
+
         Menu menuFile = new Menu("File");
         List<String> menuNames = Arrays.asList("New", "Open", "Save", "SaveAs", "Exit");
         List<MenuItem> menuItems = new ArrayList<>();
+        EventHandler<ActionEvent> action = act();
         for(var name : menuNames){
             var menuItem = new MenuItem(name);
+            menuItem.setOnAction(action);
             menuItems.add(menuItem);
             menuFile.getItems().add(menuItem);
         }
@@ -47,6 +56,38 @@ public class App extends Application {
         Scene scene = new Scene(vBox, 960, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private EventHandler<ActionEvent> act() {
+        return new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                MenuItem mItem = (MenuItem) actionEvent.getSource();
+                String name = mItem.getText();
+                switch(name){
+                    case "New":
+                        System.out.println(name + "pressed");
+                        menuBarController.actionNew();
+                        break;
+                    case "Load":
+                        System.out.println(name + "pressed");
+                        menuBarController.actionLoad();
+                        break;
+                    case "Save":
+                        System.out.println(name + "pressed");
+                        menuBarController.actionSave();
+                        break;
+                    case "SaveAs":
+                        System.out.println(name + "pressed");
+                        menuBarController.actionSaveAs();
+                        break;
+                    case "Exit":
+                        System.out.println(name + "pressed");
+                        menuBarController.actionExit();
+                        break;
+                }
+            }
+        };
     }
 
     static void setRoot(String fxml) throws IOException {
---
+++ hellofx/src/main/java/org/openjfx/MenuBarController.java
@@ -0,0 +1,20 @@
+package org.openjfx;
+
+import java.io.IOException;
+
+public class MenuBarController {
+    public void actionNew(){
+    }
+
+    public void actionLoad() {
+    }
+
+    public void actionSave() {
+    }
+
+    public void actionSaveAs() {
+    }
+
+    public void actionExit() {
+    }
+}
